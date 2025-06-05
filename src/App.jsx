52a9e107001396cc7cd2c1c439c8d7ff9e6f517a
w30r <mr.meorsyamil@gmail.com>
2025-06-05 15:42:53 +0800

    fix: api calls were not using the correct base url

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <MovieProvider>
       <div className="nav fixed top-0 left-0 w-full bg-white flex items-center justify-center gap-4 z-100 h-10">
-        <a href="/home">Home</a>
+        <a href="/">Home</a>
         <a href="/favorites">Favorites</a>
         <a href="/genres">Genres</a>
         <a href="/swipe-login">
@@ -22,7 +22,7 @@ function App() {
         </a>
       </div>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/genres" element={<Genres />} />
         <Route path="/detail/:id" element={<MovieDetail />} />
